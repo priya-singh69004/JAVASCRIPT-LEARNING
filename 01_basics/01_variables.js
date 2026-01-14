396985c610d2b636450510e1927cdeda9c6a8b26
priya-singh69004 <priyasingh69004@gmail.com>
2026-01-14 15:30:35 +0000

    Main

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountID = 144553;
+let accountEmail = "priya@23456";
+accountPassword = "12345";
+let accountState;
+
+//accountID = 325287;
+accountEmail = "priya@3456";
+
+console.table([accountID , accountEmail , accountPassword , accountState ]);
