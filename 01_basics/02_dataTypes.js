bb73c269ae9875f0a2c17feed1016b4767a1212e
priya-singh69004 <priyasingh69004@gmail.com>
2026-01-15 09:29:02 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+//alert("hello");
+
+/* Primitive Data Type
+number => 2 to the power 53
+bigInt
+string => ""
+boolean => true/false
+null => standalone value
+undefined => value is not assigned
+synbol => for uniqueness
+*/
+
+
+let age = 32;
+console.log(typeof age);
+console.log(typeof "priya");
+console.log(typeof null);
+console.log(typeof undefined);
+
