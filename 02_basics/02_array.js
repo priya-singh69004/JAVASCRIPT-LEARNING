210d6f1358d6090114eb1b64c58749904dfcb7bc
priya-singh69004 <priyasingh69004@gmail.com>
2026-01-17 14:00:30 +0000

    Array part2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,29 @@
+// ++++++++++++++ Array Method [isArray, from, of] +++++++++++++++
+
+const marvel_heros = ["thor" , "ironman" , "spiderman"]
+const dc_heros = ["superman" , "flash" , "batman"]
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros[3][1]);
+
+
+// const allHeros = marvel_heros.concat(dc_heros);
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros];  // Spread
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6],  7, [6, 7, [4, 5]]];
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
+
+
+console.log(Array.isArray("PRIYA"));
+console.log(Array.from("PRIYA"));
+console.log(Array.from({name : "priya"})); //return empty string
+
+let score1 = 100;
+let score2 = 100;
+let score3 = 300;
+
+console.log(Array.of(score1, score2 , score3));
