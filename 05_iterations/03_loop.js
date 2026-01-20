3b1062ca7983eb734557722fadbeca68ef0d343d
priya-singh69004 <priyasingh69004@gmail.com>
2026-01-20 08:53:27 +0000

    for of loop

---
+++ 05_iterations/03_loop.js
@@ -0,0 +1,43 @@
+// ++++++++ for of ++++++++++
+
+const arr = [1 , 2 , 3 ,4 ,5]
+
+for(const num of arr){
+    // console.log(num);
+}
+
+const greetings = "Hello World"
+for(const greet of greetings){
+    if(greet == " "){
+        // break;
+        continue;
+    }
+    // console.log(`Each chr is ${greet}`);
+    
+}
+
+
+// ++++++++++++++ Map +++++++++++
+
+const map = new Map()
+map.set('IN' , 'INDIA')
+map.set('USA' , 'UNITED STATE OF AMERICA')
+map.set('Fr' , 'FRANCE')
+// console.log(map);
+
+for(const key of map){
+    console.log(key);
+}
+
+// map destructure
+for(const [key , value] of map){
+    // console.log(key , ":-" , value);
+}
+
+const myObject = {
+    game1 : 'NFS',
+    game2: 'Spiderman'
+}
+for (const [key , value] of myObject) {
+    console.log(key , ":-" , value);
+}
