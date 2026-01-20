5bef50771e23ddb09641b2f64217e505d9ce98c6
priya-singh69004 <priyasingh69004@gmail.com>
2026-01-20 09:09:26 +0000

    for in loop

---
+++ 05_iterations/04_four_object.js
@@ -0,0 +1,29 @@
+// +++++++++++++ for in ++++++++++++++
+const myObject = {
+    js : "javascript",
+    cpp : 'C++',
+    rb : "ruby",
+    swift : "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js" , "rb" , "java" , "py" ,"cpp"]
+for(const key in programming){
+    // console.log(programming[key]);
+    
+}
+
+// const map = new Map()
+// map.set('IN' , 'INDIA')
+// map.set('USA' , 'UNITED STATE OF AMERICA')
+// map.set('Fr' , 'FRANCE')
+
+// for(const key in map){
+//     console.log(key);
+    
+// }
+
+
